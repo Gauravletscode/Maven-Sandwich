6b6e80bd45b02b4861d1767bc88d15365f273b9f
Harshad Kode <harshadkode5@gmail.com>
2025-06-10 14:46:18 +0200

    Enum fix

--- src/Models/OfficeManager.java
+++ src/Models/OfficeManager.java
@@ -1,7 +1,9 @@
 package Models;
 
+import Enum.Gender;
+
 public class OfficeManager extends Person {
-    public OfficeManager(String lastName, String firstName, Enum gender) {
+    public OfficeManager(String lastName, String firstName, Gender gender) {
         super(lastName, firstName, gender);
     }
     public void printOrder(){
